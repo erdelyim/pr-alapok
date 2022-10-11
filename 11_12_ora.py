6329d2f85bdb83aec2aa241e3a055dfe6f885a93
erdelyim <90777816+erdelyim@users.noreply.github.com>
2022-10-11 09:40:15 +0200

    Add files via upload

---
+++ 11_12_ora.py
@@ -0,0 +1,43 @@
+#feladat1
+"""
+gondolt_szám = 4
+tipp = input('Gondoltam egy számra. Tippled meg! ')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print("Ez az igaz ág!")
+print("páápáá.")
+
+"""
+
+#feladat2
+"""
+gondolt_szám = 4
+tipp = input('Gondoltam egy számra. Tippled meg! ')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print("YAEHH!")
+    print("Komoly vagy.")
+else:
+    print('Hosszan gondolkodtal rajta?:)')
+    print('Nem érte meg te hülyeee.;)')
+print('Csáóóóó.')
+
+"""
+
+szam1=input('kérek egy számot')
+szam1=int(szam1)
+szam2=input('kérek egy másik számot')
+szam2=int(szam2)
+
+if szam1 == szam2:
+    print("a két szám megegyezik ")
+
+if szam1 != szam2:
+    print("a két szám nem egyezik meg")
+
+print("a program vége")
+
+if szam1 >= szam2:
+    print('számod kisebb')
+else:
+    print('nagyobb vagy egyenlő')
